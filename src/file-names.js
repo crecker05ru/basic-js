5eb4d878d378c655b28ff0df4ffebf66f22b49ce
Anvar <fort2652@gmail.com>
2023-04-17 00:12:15 +0300

    file-names done

--- src/file-names.js
+++ src/file-names.js
@@ -15,10 +15,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
+function renameFiles(names) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+ let count = 0
+ let newArr = []
+ for(let i = 0;i < names.length;i++){
+  if(!newArr.includes(names[i])){
+    newArr.push(names[i])
+    count = 0
+  }else {
+    count ++
+    names[i] = count > 0 ? `${names[i]}(${count})` : names[i]
+    newArr.push(names[i])
+  }
+ }
+ console.log('names',names)
+ console.log('newArr',newArr)
+ return names
+//  let map = {}
+//  for(let i = 0;i < names.length;i++){
+//   if(map[names[i]]){
+//     map[names[i]] += 1 
+//   }else {
+//     map[names[i]] = 1
+//   }
+//  }
+
+//   console.log('map',map)
+//  return names.map(name => {
+//   if(map[name] > 1){
+//     return `${name}(${map[name]})`
+//   }else{
+//     return name
+//   }
+//  })
 }
+
+console.log("['doc', 'doc(1)', 'image', 'doc(1)(1)', 'doc(2)']",renameFiles(['doc', 'doc', 'image', 'doc(1)', 'doc']))
 
 module.exports = {
   renameFiles
